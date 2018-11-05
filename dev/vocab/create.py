8a51b236c977bf85523e61f71769ca497a4991bb
patarapolw <patarapolw@gmail.com>
2018-11-05 20:56:48 +0700

    Update vocab list to prioritize existing Hanzi

--- dev/vocab/create.py
+++ dev/vocab/create.py
@@ -1,23 +1,71 @@
 from ruamel import yaml
-from zhlib.util import sort_vocab
-import math
+import regex
 
 
-with open('hsk.yaml') as f_in, open('generated.yaml', 'a') as f_out:
+with open('hsk.yaml') as f_in, open('generated.yaml', 'w') as f_out, open('../hanzi/generated.yaml') as h_rule:
     d = yaml.safe_load(f_in)
+    rule = yaml.safe_load(h_rule)
+
     d_out = dict()
-    hsk_level = 6
-    source = f'HSK{hsk_level}'
-    vocab = sort_vocab(d[source])
-    print(len(vocab))
-    for i in range(0, 20):
-        level = str(40 + i + 1)
-        chuck = math.ceil(len(vocab) / 20)
+
+    prev = None
+    used = set()
+
+    for i in range(60):
+        level = '{:02d}'.format(i+1)
+        allowed_hanzi = rule[level][0]['hanzi']
+
+        if i < 5:
+            hsk = 1
+            no_vocab = 30
+        elif i < 10:
+            hsk = 2
+            no_vocab = 30
+        elif i < 20:
+            hsk = 3
+            no_vocab = 30
+        elif i < 30:
+            hsk = 4
+            no_vocab = 60
+        elif i < 40:
+            hsk = 5
+            no_vocab = 130
+        else:
+            hsk = 6
+            no_vocab = 125
+
+        source = f'HSK{hsk}'
+        v_list = d[source]
+
+        current_vocab = set()
+        for vocab in v_list:
+            if vocab not in used:
+                if all(h in allowed_hanzi for h in regex.findall(r'\p{IsHan}', vocab)):
+                    current_vocab.add(vocab)
+                    if len(current_vocab) >= no_vocab:
+                        break
+                used.add(vocab)
+
+        v_list = [v for v in v_list if v not in current_vocab]
+
+        if len(current_vocab) < no_vocab:
+            limit = no_vocab - len(current_vocab)
+            for x, vocab in enumerate(v_list):
+                if x < limit:
+                    current_vocab.add(vocab)
+                    used.add(vocab)
+                else:
+                    break
+
+        v_list = [v for v in v_list if v not in current_vocab]
+
+        d[source] = v_list
 
         d_out.setdefault(level, list()).append({
             '_meta': {
                 'source': source
             },
-            'vocab': vocab[i*chuck: (i+1)*chuck]
+            'vocab': list(current_vocab)
         })
+
     yaml.safe_dump(d_out, f_out, allow_unicode=True)
